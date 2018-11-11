2070d9e3254cd6f7eca9eeea1d0cdf45fa8cd417
marjune <marjune@163.com>
2018-11-11 22:46:43 +0800

    feat: upgrade to jquery-tab version 3

--- webpack-ts/src/ts/main.js
+++ webpack-ts/src/ts/main.js
@@ -5,6 +5,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var jquery_1 = __importDefault(require("jquery"));
 require("jquery-tab");
-require("jquery-tab/src/built/theme/gray");
+require("jquery-tab/src/built/style/theme/gray");
 require("../css/main.css");
 jquery_1.default('.tab1').tab();
